4796d7859ea23ed6303dee1af45ca6bbad1f9ca3
tonimjdev <tonimdev@gmail.com>
2022-11-14 12:42:28 +0100

    382 - Renovar y validar el JWT

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
@@ -20,7 +21,7 @@ router.post( '/', [
 ] ,loginUsuario );
 
 // Validar y revalidar token
-router.get( '/renew', revalidarToken );
+router.get( '/renew', validarJWT, revalidarToken );
 
 
 
