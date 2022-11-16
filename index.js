429dcd4033475057b3530f6e251544bc77e809a7
tonimjdev <tonimdev@gmail.com>
2022-11-16 11:46:22 +0100

    407 - Desplegar aplicación Angular en Node

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
@@ -23,6 +24,12 @@ app.use( express.json() );
 // Rutas
 app.use( '/api/auth', require('./routes/auth') );
 
+// Manejar demas rutas
+app.get( '*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) )
+});
+
+
 app.listen( process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 })
